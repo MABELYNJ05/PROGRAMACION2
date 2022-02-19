06325bb46d967e92a8b3fe5eee9ceb68f7845a05
Mabelyn Crúz <cmabelynj@gmail.com>
2022-02-19 12:33:00 -0600

    PROGRAMACION2

---
+++ PROGRA3-EJERCICIOS/Ejercicio2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int[] arreglo = { 1, 2, 3, 4, 5 };
+            int punteo = 0;
+
+            Console.Write("Arreglo dado: ");
+
+            for (int i = 0; i < arreglo.Length; i++)
+            {                
+                Console.WriteLine("\t"+arreglo[i]);
+                if ((arreglo[i] % 2) != 0)
+                {
+                    if (arreglo[i] == 5)
+                    {
+                        punteo += 5;
+                    }
+                    else
+                    {
+                        punteo += 3;
+                    }
+                }
+                else
+                {
+                    punteo++;
+                }
+
+            }
+
+            Console.WriteLine("Punteo total: " + punteo);
+
+        }
+    }
+}
---
+++ PROGRA3-EJERCICIOS/SecuenciaTrobinacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SecuenciaTrobinacci
+{
+    class Program
+    {        
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 0, c = 1;
+            int d = 0;
+            Console.WriteLine("Secuencia Tribonacci: ");
+            Console.WriteLine(a + "\n" + b + "\n" + c);
+
+            for(int i =3; i<num; i++)
+            {
+                d = a + b + c;
+                Console.WriteLine(d);
+                a = b;
+                b = c;
+                c = d;
+            }
+
+        }
+    }
+}
